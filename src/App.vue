6bc80b656f3494b960d02826fcd21424aec530d9
Noga <nogaran01@gmail.com>
2022-10-07 14:20:19 +0300

    changes

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script >
+  import { RouterLink, RouterView } from 'vue-router'
+  import headerVue from './cmps/header.vue'
+  import footerVue from './cmps/footer.vue';
+  
+  export default {
+    components: {
+      RouterLink,
+      RouterView,
+      headerVue,
+      footerVue
+    },
+  }
+</script>
+  
+<template>
+  <headerVue />
+  <RouterView v-slot="{Component}">
+    <transition name="fade" mode="out-in">
+      <component :is="Component"></component>
+    </transition>
+  </RouterView>
+  <footerVue/>
+</template>
---
+++ src/cmps/footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <footer>
+        <section>
+            &copy;&nbsp;<span v-for="letter in ['S','u','n','F','l','o','w','e','r']"><p>{{letter}}</p></span>&nbsp;{{year}}
+        </section>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'footer',
+    data() {
+        return {
+            year: (new Date).getFullYear()
+        }
+    },
+}
+</script>
